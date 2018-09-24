0b926abe84d68f1f8be170eece47aba2c1e57402
Chrystyan Byrnes <chrystyan96@hotmail.com>
2018-09-24 15:03:04 -0300

    add a opção para digitar o ip no lado do cliente

--- src/SRC_Game/Client.java
+++ src/SRC_Game/Client.java
@@ -1,7 +1,9 @@
 
 package SRC_Game;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -9,9 +11,15 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket client = new Socket("127.0.0.1", 1234);
-            Scanner in = new Scanner(client.getInputStream());
             Scanner inUser = new Scanner(System.in);
+            System.out.println("Digite o IP para se conectar (padrão 127.0.0.1): ");
+            BufferedReader ipReader = new BufferedReader(new InputStreamReader(System.in)); 
+            String ip = ipReader.readLine();
+            if(ip == "") {
+                ip = "127.0.0.1";
+            }
+            Socket client = new Socket(ip, 1234);
+            Scanner in = new Scanner(client.getInputStream());
             PrintWriter out = new PrintWriter(client.getOutputStream(),true);
             Client.stringSplit(in);
 
